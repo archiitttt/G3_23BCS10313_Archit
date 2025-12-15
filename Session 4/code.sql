e5257dd86953cf213d1df68f47544617537e84c3
Archit Kaushal <architkaushal2004@gmail.com>
2025-12-15 09:53:50 +0530

    Add files via upload

---
+++ Session 4/code.sql
@@ -0,0 +1,20 @@
+SELECT 
+    ROUND(
+        100.0 * SUM(
+            CASE 
+                WHEN d.order_date = d.customer_pref_delivery_date THEN 1 
+                ELSE 0 
+            END
+        ) / COUNT(*),
+        2
+    ) AS immediate_percentage
+FROM Delivery d
+JOIN (
+    SELECT customer_id, MIN(order_date) AS first_order_date
+    FROM Delivery
+    GROUP BY customer_id
+) first
+ON d.customer_id = first.customer_id
+AND d.order_date = first.first_order_date;
+
+--G3 23BCS10313 Archit Kaushal
